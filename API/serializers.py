4ead5980001143b2d2f4f6e5947d726f7fb03005
Mateusz Natoniewski <natoniewski.m@gmail.com>
2019-04-02 17:27:50 +0200

    Api functions

--- API/serializers.py
+++ API/serializers.py
@@ -6,3 +6,9 @@ class ExamSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Exam
         fields = '__all__'
+
+    def validate(self, attrs):
+        """ Validation for exam points (Limit is set to 100) """
+        if attrs['points'] > 100:
+            raise serializers.ValidationError('Maximum value of points is 100')
+        return attrs
--- API/views.py
+++ API/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import ExamSerializer
 from .models import Exam
+from rest_framework.response import Response
+from rest_framework import status
 
 
 # Create your views here.
@@ -9,3 +10,9 @@ from .models import Exam
 class ExamViewSet(viewsets.ModelViewSet):
     queryset = Exam.objects.all()
     serializer_class = ExamSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        """ Additional function to delete instance """
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
