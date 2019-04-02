caa060fdbf9d1127cae41bcce765a9a0e68f6adc
Mateusz Natoniewski <natoniewski.m@gmail.com>
2019-04-02 22:58:44 +0200

    Owner permissions

--- API/views.py
+++ API/views.py
@@ -1,21 +1,16 @@
 from rest_framework import viewsets
 from .serializers import TaskSerializer, ExamSerializer
 from .models import Exam, Task
-from rest_framework.response import Response
-from rest_framework import status
+from .permissions import ExamPermission, TaskPermission
 
 
 class TaskViewSet(viewsets.ModelViewSet):
+    permission_classes = (TaskPermission,)
     queryset = Task.objects.all().order_by('-points', 'exam__name', 'task')
     serializer_class = TaskSerializer
 
-    def destroy(self, request, *args, **kwargs):
-        """ Additional function to delete instance """
-        instance = self.get_object()
-        self.perform_destroy(instance)
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
 
 class ExamViewSet(viewsets.ModelViewSet):
+    permission_classes = (ExamPermission,)
     queryset = Exam.objects.all().order_by('-grade')
     serializer_class = ExamSerializer
